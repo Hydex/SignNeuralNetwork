6cd9d955b6e327b7b84b47df95773aa62be6eb70
wso277 <oliveira.wilson93@gmail.com>
2014-04-14 14:24:25 +0100

    mediumGesture

--- src/mieic/iart/SignLangNN/backend/Sample.java
+++ src/mieic/iart/SignLangNN/backend/Sample.java
@@ -7,23 +7,60 @@ import java.util.ArrayList;
  */
 public class Sample {
 
-    String name;
-    ArrayList<HandGesture> rightHandGestures;
-    ArrayList<HandGesture> leftHandGestures;
+    private String name;
+    private ArrayList<HandGesture> rightHandGestures;
+    private ArrayList<HandGesture> leftHandGestures;
+    private Float[] mediumGesture;
 
     public Sample(String newName) {
         name = newName;
         rightHandGestures = new ArrayList<>();
         leftHandGestures = new ArrayList<>();
+        mediumGesture = new Float[22];
     }
 
     public Integer processName() {
-        int hash=7;
-        for (int i=0; i < name.length(); i++) {
-            hash = hash*31+name.charAt(i);
+        int hash = 7;
+        for (int i = 0; i < name.length(); i++) {
+            hash = hash * 31 + name.charAt(i);
         }
 
         return hash;
+    }
+
+    public void processGesture() {
+        float[] values = new float[22];
+
+        int i = 0;
+        for (; i < leftHandGestures.size(); i++) {
+            values[0] += leftHandGestures.get(i).getX();
+            values[1] += leftHandGestures.get(i).getY();
+            values[2] += leftHandGestures.get(i).getZ();
+            values[3] += leftHandGestures.get(i).getRoll();
+            values[4] += leftHandGestures.get(i).getPitch();
+            values[5] += leftHandGestures.get(i).getYaw();
+            values[6] += leftHandGestures.get(i).getThumbBend();
+            values[7] += leftHandGestures.get(i).getForefingerBend();
+            values[8] += leftHandGestures.get(i).getMiddlefingerBend();
+            values[9] += leftHandGestures.get(i).getRingfingerBend();
+            values[10] += leftHandGestures.get(i).getLittlefingerBend();
+
+            values[11] += rightHandGestures.get(i).getX();
+            values[12] += rightHandGestures.get(i).getY();
+            values[13] += rightHandGestures.get(i).getZ();
+            values[14] += rightHandGestures.get(i).getRoll();
+            values[15] += rightHandGestures.get(i).getPitch();
+            values[16] += rightHandGestures.get(i).getYaw();
+            values[17] += rightHandGestures.get(i).getThumbBend();
+            values[18] += rightHandGestures.get(i).getForefingerBend();
+            values[19] += rightHandGestures.get(i).getMiddlefingerBend();
+            values[20] += rightHandGestures.get(i).getRingfingerBend();
+            values[21] += rightHandGestures.get(i).getLittlefingerBend();
+        }
+
+        for (int j=0; j < mediumGesture.length;j++) {
+            mediumGesture[j] = values[j] / (float)i;
+        }
     }
 
     public void addLeftGesture(HandGesture gesture) {
