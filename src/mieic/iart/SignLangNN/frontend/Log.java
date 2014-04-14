e0c9d7d1b8c7fc8905a139aa5af0070ddd89c094
knoweat <diogojapinto@gmail.com>
2014-04-14 15:10:31 +0100

    added Log class

---
+++ src/mieic/iart/SignLangNN/frontend/Log.java
@@ -0,0 +1,18 @@
+package mieic.iart.SignLangNN.frontend;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by knoweat on 14/04/14.
+ */
+public class Log {
+    public static boolean PRINT_LOGS = true;
+
+    public static void log(String messg) {
+        if (PRINT_LOGS) {
+            java.util.Date date = new java.util.Date();
+            System.out.println(new Timestamp(date.getTime()) + "=>" + messg);
+        }
+    }
+
+}
